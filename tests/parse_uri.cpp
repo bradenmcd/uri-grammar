fd222ef0e2c9f7283bb6dd60fc59f10987c1aba0
Braden McDaniel <braden@endoframe.com>
2006-01-02 23:49:01 +0000

    Use GNU Argp instead of Boost.ProgramOptions. ProgramOptions in Boost 1.33.1 has a bug that prevents programs from having options that start with the same sequence of characters followed by a dash (e.g., "--foo" and "--foo-bar").

--- tests/parse_uri.cpp
+++ tests/parse_uri.cpp
@@ -18,102 +18,187 @@
 //
 
 # include <iostream>
-# include <boost/program_options.hpp>
+# include <argp.h>
 # include <uri/grammar.hpp>
 
-class print_actions {
-public:
-    struct action {
-        explicit action(const bool print):
-            print_(print)
-        {}
+extern "C" {
+    const char * argp_program_version = "parse-uri " PACKAGE_VERSION;
+    const char * argp_program_bug_address = PACKAGE_BUGREPORT;
 
-        template <typename Iterator>
-        void operator()(const Iterator & first,
-                        const Iterator & last) const
-        {
-            if (this->print_) {
-                std::cout << std::string(first, last) << std::endl;
-            }
-        }
+    error_t parse_opt(int key, char * arg, argp_state * state);
+}
 
-    private:
-        bool print_;
+namespace {
+
+    enum option_id {
+        scheme_id = 128,
+        scheme_specific_part_id,
+        userinfo_id,
+        host_id,
+        port_id,
+        authority_id,
+        path_id,
+        query_id,
+        fragment_id
     };
 
-    print_actions(const bool print_scheme,
-                  const bool print_scheme_specific_part,
-                  const bool print_userinfo,
-                  const bool print_host,
-                  const bool print_port,
-                  const bool print_authority,
-                  const bool print_path,
-                  const bool print_query,
-                  const bool print_fragment):
-        scheme(print_scheme),
-        scheme_specific_part(print_scheme_specific_part),
-        userinfo(print_userinfo),
-        host(print_host),
-        port(print_port),
-        authority(print_authority),
-        path(print_path),
-        query(print_query),
-        fragment(print_fragment)
-    {}
+    argp_option options[] = {
+        { "scheme", scheme_id, 0, 0, "Print the scheme", 0 },
+        { "scheme-specific-part", scheme_specific_part_id, 0, 0,
+          "Print the scheme-specific part", 0 },
+        { "userinfo", userinfo_id, 0, 0, "Print the user information", 0 },
+        { "host", host_id, 0, 0, "Print the host", 0 },
+        { "port", port_id, 0, 0, "Print the port", 0 },
+        { "authority", authority_id, 0, 0, "Print the authority", 0 },
+        { "path", path_id, 0, 0, "Print the path", 0 },
+        { "query", query_id, 0, 0, "Print the query", 0 },
+        { "fragment", fragment_id, 0, 0, "Print the fragment", 0 },
+        { 0, 0, 0, 0, 0, 0 }
+    };
 
-    action scheme, scheme_specific_part, userinfo, host, port, authority, path,
-                             query, fragment;
-};
+    struct arguments {
+        bool print_scheme, print_scheme_specific_part, print_userinfo,
+            print_host, print_port, print_authority, print_path, print_query,
+            print_fragment;
+        std::string uri;
+
+        arguments():
+            print_scheme(false),
+            print_scheme_specific_part(false),
+            print_userinfo(false),
+            print_host(false),
+            print_port(false),
+            print_authority(false),
+            print_path(false),
+            print_query(false),
+            print_fragment(false)
+        {}
+    };
+
+    class print_actions {
+    public:
+        struct action {
+            explicit action(const bool print):
+                print_(print)
+            {}
+
+            template <typename Iterator>
+            void operator()(const Iterator & first,
+                            const Iterator & last) const
+            {
+                if (this->print_) {
+                    std::cout << std::string(first, last) << std::endl;
+                }
+            }
+
+        private:
+            bool print_;
+        };
+
+        print_actions(const bool print_scheme,
+                      const bool print_scheme_specific_part,
+                      const bool print_userinfo,
+                      const bool print_host,
+                      const bool print_port,
+                      const bool print_authority,
+                      const bool print_path,
+                      const bool print_query,
+                      const bool print_fragment):
+            scheme(print_scheme),
+            scheme_specific_part(print_scheme_specific_part),
+            userinfo(print_userinfo),
+            host(print_host),
+            port(print_port),
+            authority(print_authority),
+            path(print_path),
+            query(print_query),
+            fragment(print_fragment)
+        {}
+
+        action scheme, scheme_specific_part, userinfo, host, port, authority,
+                                 path, query, fragment;
+    };
+}
 
 int main(int argc, char * argv[])
-try {
-    using namespace boost::program_options;
+{
     using namespace boost::spirit;
     using namespace std;
 
-    options_description desc("Options");
-    desc.add_options()
-        ("help,h", "display this help and exit")
-        ("scheme", "print scheme of URI")
-        ("scheme-specific-part", "print scheme-specific part of URI")
-        ("userinfo", "print userinfo of URI")
-        ("host", "print host of URI")
-        ("port", "print port of URI")
-        ("authority", "print authority of URI")
-        ("path", "print path of URI")
-        ("query", "print query of URI")
-        ("fragment", "print fragment of URI")
-        ;
-    parsed_options parsed = parse_command_line(argc, argv, desc);
-    variables_map vm;
-    store(parsed, vm);
-    notify(vm);
-    if (vm.count("help")) {
-        cout << "Usage: " << argv[0] << " [OPTION]... URI" << endl
-             << "Print the different parts of URI." << endl
-             << endl
-             << desc
-             << endl
-             << "Report bugs to <" << PACKAGE_BUGREPORT << '>' << endl;
-        return EXIT_SUCCESS;
-    }
+    arguments arguments;
+    char args_doc[] = "URI";
+    char * const doc = 0;
+    argp_child * const children = 0;
+    char *(*help_filter)(int key, const char * text, void * input) = 0;
+    char * argp_domain = 0;
+    argp argp = {
+        options,
+        parse_opt,
+        args_doc,
+        doc,
+        children,
+        help_filter,
+        argp_domain
+    };
+    int uri_arg_index;
+    argp_parse(&argp, argc, argv, 0, &uri_arg_index, &arguments);
 
-    print_actions actions(vm.count("scheme"),
-                          vm.count("scheme-specific-part"),
-                          vm.count("userinfo"),
-                          vm.count("host"),
-                          vm.count("port"),
-                          vm.count("authority"),
-                          vm.count("path"),
-                          vm.count("query"),
-                          vm.count("fragment"));
+    print_actions actions(arguments.print_scheme,
+                          arguments.print_scheme_specific_part,
+                          arguments.print_userinfo,
+                          arguments.print_host,
+                          arguments.print_port,
+                          arguments.print_authority,
+                          arguments.print_path,
+                          arguments.print_query,
+                          arguments.print_fragment);
 
-    const string uri = argv[parsed.options.size()];
-    uri::uri_grammar<print_actions> g(actions);
-    if (!parse(uri.begin(), uri.end(), g, space_p).full) {
+    if (arguments.uri.empty()) {
+        cerr << argv[0] << ": required URI argument not given" << endl;
         return EXIT_FAILURE;
     }
-} catch (boost::program_options::unknown_option & ex) {
-    std::cerr << argv[0] << ": " << ex.what() << std::endl;
-    return EXIT_FAILURE;
+    uri::uri_grammar<print_actions> g(actions);
+    if (!parse(arguments.uri.begin(), arguments.uri.end(), g, space_p).full) {
+        return EXIT_FAILURE;
+    }
+}
+
+error_t parse_opt(int key, char * arg, argp_state * state)
+{
+    arguments & args = *static_cast<arguments *>(state->input);
+
+    switch (key) {
+    case scheme_id:
+        args.print_scheme = true;
+        break;
+    case scheme_specific_part_id:
+        args.print_scheme_specific_part = true;
+        break;
+    case userinfo_id:
+        args.print_userinfo = true;
+        break;
+    case host_id:
+        args.print_host = true;
+        break;
+    case port_id:
+        args.print_port = true;
+        break;
+    case authority_id:
+        args.print_authority = true;
+        break;
+    case path_id:
+        args.print_path = true;
+        break;
+    case query_id:
+        args.print_query = true;
+        break;
+    case fragment_id:
+        args.print_fragment = true;
+        break;
+    case ARGP_KEY_ARG:
+        args.uri = arg;
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
 }
