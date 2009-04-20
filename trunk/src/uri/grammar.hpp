f7c05969f4ae076c348c558a687e7de9c1f5205a
braden.mcdaniel <braden.mcdaniel@2a277f4e-ab19-0410-adff-8f8ff343f446>
2009-04-20 01:02:40 +0000

    Changed license to the Boost license.

--- trunk/src/uri/grammar.hpp
+++ trunk/src/uri/grammar.hpp
@@ -2,21 +2,12 @@
 //
 // uri_grammar
 //
-// Copyright 2006  Braden McDaniel
+// Copyright 2009  Braden McDaniel
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
+// Distributed under the Boost Software License, Version 1.0.
 //
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// See accompanying file COPYING or copy at
+// http://www.boost.org/LICENSE_1_0.txt
 //
 
 # ifndef URI_GRAMMAR_HPP
